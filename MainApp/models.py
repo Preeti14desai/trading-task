9f40ba911bae9aa8deb91bbdf95d1a54674e765d
preeti14desai <preetipujari6@gmail.com>
2023-10-25 19:04:14 +0530

    initial commit

---
+++ MainApp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Candle(models.Model):
+    id = models.AutoField(primary_key=True)
+    open = models.DecimalField(max_digits=10, decimal_places=2)
+    high = models.DecimalField(max_digits=10, decimal_places=2)
+    low = models.DecimalField(max_digits=10, decimal_places=2)
+    close = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField()
